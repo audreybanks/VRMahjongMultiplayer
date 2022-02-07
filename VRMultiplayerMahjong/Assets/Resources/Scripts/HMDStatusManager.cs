7d6ae91246d808b0e96e84dbe46bde3fc8d4a70a
audreybanks <68301420+audreybanks@users.noreply.github.com>
2022-02-07 16:46:54 -0500

    Finished adding all the tile prefabs

--- VRMultiplayerMahjong/Assets/Resources/Scripts/HMDStatusManager.cs
+++ VRMultiplayerMahjong/Assets/Resources/Scripts/HMDStatusManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using UnityEngine.XR;
 
 public class HMDStatusManager : MonoBehaviour {
+
+    public GameObject XRDeviceSimulator;
+
     // Start is called before the first frame update
     void Start() {
-        Debug.Log("HMD Name: " + XRSettings.loadedDeviceName);
+        if (XRSettings.loadedDeviceName == "MockHMD Display") {
+            XRDeviceSimulator.SetActive(true);
+        }
     }
 }
